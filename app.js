bd69acbaea0dce4db5d22c0e08b019875a25f8e0
Md. Hanif <mdhanif6731@gmail.com>
2020-07-16 21:12:38 +0600

    Numeric calc

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 (function () {
 'use strict';
-angular.module('testAngularApp',[])
-    .controller('TestAngularController',function ($scope) {
-        $scope.name='Hanif';
-        $scope.sayHello=function () {
-            return 'Hello Hanif';
-        }
+angular.module('NameCalculatorApp',[])
+    .controller('NameCalculatorController',function ($scope) {
+        $scope.name="";
+        $scope.totalLen=0;
+        $scope.displayNumeric=function () {
+            let totalNameValue=calculateNameLen($scope.name);
+            $scope.totalLen=totalNameValue;
+
+        };
+        function calculateNameLen(str) {
+            var tsValue=0;
+            for(var k=0; k<str.length; k++){
+                tsValue += str.charCodeAt(k);
+            }
+            return tsValue;
+        };
     });
 })();
